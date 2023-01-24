20662e123e0b1f60abfe9f50a3cb5995f79b8020
Jessie <jessica.v.rid@gmail.com>
2023-01-24 12:41:32 +0300

    Task2_delete_2nd_digit

---
+++ Task2_3_digit_num_delete_2d/Program.cs
@@ -0,0 +1,6 @@
+﻿Random rand = new Random();
+int randnumber = rand.Next(100, 1000);
+System.Console.WriteLine(randnumber);
+int seconddigit = randnumber%10;
+int firstdigit = randnumber/100;
+System.Console.WriteLine($"{firstdigit}{seconddigit}");
