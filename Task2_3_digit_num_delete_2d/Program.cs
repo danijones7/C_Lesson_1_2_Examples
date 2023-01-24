8bd56739c93e47c62cb501915456ff60920ba768
Jessie <jessica.v.rid@gmail.com>
2023-01-24 15:32:49 +0300

    Task3_USEFUL stuff

--- Task2_3_digit_num_delete_2d/Program.cs
+++ Task2_3_digit_num_delete_2d/Program.cs
@@ -1,6 +1,10 @@
 ﻿Random rand = new Random();
 int randnumber = rand.Next(100, 1000);
 System.Console.WriteLine(randnumber);
-int seconddigit = randnumber%10;
-int firstdigit = randnumber/100;
-System.Console.WriteLine($"{firstdigit}{seconddigit}");
+//int seconddigit = randnumber % 10;
+//int firstdigit = randnumber / 100;
+//System.Console.WriteLine($"{firstdigit}{seconddigit}");
+
+int newnumber = (randnumber / 100) * 10 + randnumber % 10; // если это число необходимо использовать в дальнейшем лучше вычислить его математически 
+System.Console.WriteLine(newnumber);
+
